902a6fe21ed232657f2d69225cba99e2df65b5e1
nathalia-santos <nathaliasantospbg@hotmail.com>
2022-10-27 10:39:16 -0300

    exercicio laco multiplo de 3

---
+++ sintaxe-variaveis-e-fluxo/src/Multiplos3.java
@@ -0,0 +1,13 @@
+
+public class Multiplos3 {
+	
+	public static void main(String[] args) {
+		int numero = 6;
+		if (numero % 3 == 0) {
+			System.out.println("É divisivel por 3");
+		} else {
+			System.out.println("Não é divisível por 3");
+		}
+	}
+
+}
